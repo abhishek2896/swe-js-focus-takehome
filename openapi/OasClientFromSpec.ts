fa10986f02c6f3ff75fe7cee413e4fe677ffa964
Abhishek Nagarkoti <abhisheknagarkoti@gmail.com>
2024-01-28 22:43:27 +0530

    Fix: make producer optional on mocking client endpoints

--- openapi/OasClientFromSpec.ts
+++ openapi/OasClientFromSpec.ts
@@ -89,9 +89,12 @@ export class OASClientFromSpec {
   }
 
   buildClientMethodDefinition(schema: PropertyKind[]): FunctionExpressionKind {
+    const producer = b.identifier("producer");
+    producer.optional = true;
+    producer.typeAnnotation
     return b.functionExpression(
       null,
-      [b.identifier("producer")],
+      [producer],
       b.blockStatement([
         b.variableDeclaration("const", [
           b.variableDeclarator(
@@ -101,16 +104,16 @@ export class OASClientFromSpec {
             ])
           ),
         ]),
-        b.variableDeclaration("const", [
-          b.variableDeclarator(
-            b.identifier("result"),
+        b.ifStatement(
+          b.identifier("producer"),
+          b.returnStatement(
             b.callExpression(b.identifier("produce"), [
               b.identifier("faked"),
               b.identifier("producer"),
             ])
           ),
-        ]),
-        b.returnStatement(b.identifier("result")),
+          b.returnStatement(b.identifier("faked"))
+        )
       ])
     );
   }
